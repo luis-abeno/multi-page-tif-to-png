a417d035288039923ce93eb08cbe25338fcd5623
Luis Henrique Tetsuo Abeno de Mambro <luis.mambro@onr.org.br>
2024-10-24 09:04:45 -0300

    feat: adding browser version

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "tiff2png-cache-v1";
+const urlsToCache = ["/", "/index.html", "/tiff.min.js", "/icon-192x192.png", "/icon-512x512.png"];
+
+// Install the service worker
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update the cache when new versions are deployed
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
